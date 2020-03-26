2d947e539b9d8b16a58e5354c639c4a7d3f870ef
Tanvir Ahamed <you@example.com>
2020-03-26 16:54:16 +0600

    Add ScrollUp

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,6 +174,19 @@
     $('select').niceSelect();
     // End Nice Select
 
+    //  Scroll Up
+    $.scrollUp({
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '300', // Distance from top before showing element (px)
+        topSpeed: 300, // Speed back to top (ms)
+        animation: 'fade', // Fade, slide, none
+        animationInSpeed: 200, // Animation in speed (ms)
+        animationOutSpeed: 200, // Animation out speed (ms)
+        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element
+        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
+//   Scroll Up
+
      // Smooth Scroll for IE/ EDGE/ SAFARI
      $('a').on('click', function(event){
         if (this.hash !==''){
